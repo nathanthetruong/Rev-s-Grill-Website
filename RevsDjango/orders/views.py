c5111f0ec7e90f2f9328697955ea91a34c9c0cfa
nathanthetruong <t.nathan235@gmail.com>
2024-03-27 12:37:08 -0500

    Added buttons from the database

--- RevsDjango/orders/views.py
+++ RevsDjango/orders/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse
+from django.db import connection
 
-# Create your views here.
+# # Create your views here.
+# def orders(request):
+#     return render(request, 'orders/orders.html')
+
+# Initializes all the menu items buttons
 def orders(request):
-    return render(request, 'orders/orders.html')
+    with connection.cursor() as cursor:
+        # Gets a list of all the menu items and sorts in alphabetical order
+        cursor.execute("SELECT description FROM menu_items")
+        data = cursor.fetchall()
+        data.sort()
+        buttonData = [{'description': currentItem[0]} for currentItem in data]
+
+        context = {'buttonData': buttonData}
+        return render(request, 'orders/orders.html', context)
