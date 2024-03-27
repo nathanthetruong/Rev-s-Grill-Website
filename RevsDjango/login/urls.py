197b6e00b4b9bebda0b1a0f6e59b2f9ccd0aee21
nathanthetruong <t.nathan235@gmail.com>
2024-03-27 01:00:56 -0500

    Completed SQL Connection and Testing Page Redirection

--- RevsDjango/login/urls.py
+++ RevsDjango/login/urls.py
@@ -7,5 +7,6 @@ This is the login page routing logic.
 
 urlpatterns = [
     path('', views.login, name='Revs-Login-Screen'),
+    path('authenticate-user/', views.authenticateUser, name='authenticate-user'),
     path('about/', views.about, name='Revs-About-Screen'),
 ]
