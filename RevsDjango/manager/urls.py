704d3537a1d210dd12fcddf1644db21303d43f45
AlexDo12 <alexthanhdo2003@gmail.com>
2024-03-31 13:54:15 -0500

    Add ability to add items to menu_items and framework for manager pages

--- RevsDjango/manager/urls.py
+++ RevsDjango/manager/urls.py
@@ -7,4 +7,9 @@ This is the manager page routing logic.
 
 urlpatterns = [
     path('', views.manager, name='Revs-Manager-Screen'),
+    path('excess/', views.excess, name='Revs-excess-Screen'),
+    path('productusage/', views.productusage, name='Revs-productusage-Screen'),
+    path('restock/', views.restock, name='Revs-restock-Screen'),
+    path('sales/', views.sales, name='Revs-sales-Screen'),
+    path('trends/', views.trends, name='Revs-trends-Screen'),
 ]
