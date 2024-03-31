02522c38c2cf2da8379dbfc10ce545f8930073de
AlexDo12 <alexthanhdo2003@gmail.com>
2024-03-30 19:29:51 -0500

    Manager page populated

--- RevsDjango/manager/views.py
+++ RevsDjango/manager/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MenuItems, Inventory
+
 
 # Create your views here.
 def manager(request):
-    return render(request, 'manager/manager.html')
+    menu_items = MenuItems.objects.all()
+    inventory_items = Inventory.objects.all()
+    context = {
+        'menu_items': menu_items,
+        'inventory_items': inventory_items,
+    }
+    return render(request, 'manager/manager.html', context)
