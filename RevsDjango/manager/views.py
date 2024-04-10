45f645bd28098f036367c65e20c816d39896ceaf
nathanthetruong <t.nathan235@gmail.com>
2024-04-10 15:37:54 -0500

    Implemented text submission for sales report

--- RevsDjango/manager/views.py
+++ RevsDjango/manager/views.py
@@ -4,6 +4,7 @@ from .models import MenuItems, Inventory
 from django.db import connection
 from django.utils import timezone
 from datetime import datetime, timedelta
+from django import forms
 
 # Create your views here.
 def manager(request):
@@ -51,27 +52,46 @@ def productusage(request):
     return render(request, 'manager/productusage.html')
 
 def sales(request):
-    context = {'sales_report': getYearHistory()}
+    startingDate = timezone.now().date()-timedelta(days=365)
+    endingDate = timezone.now().date()
+    startingDateForm = StartDateForm()
+    endingDateForm = EndDateForm()
+    if request.method == "POST":
+        if "submit_button" in request.POST:
+            startingDateForm = StartDateForm(request.POST)
+            endingDateForm = EndDateForm(request.POST)
+
+            # If the date is valid, extracts the selected date
+            if startingDateForm.is_valid():
+                startingDate = startingDateForm.cleaned_data['startDate']
+            if endingDateForm.is_valid():
+                endingDate = endingDateForm.cleaned_data['endDate']
+
+    sales_report = getSalesReport(startingDate, endingDate)
+
+    # Default option
+    context = {'sales_report': sales_report,
+                'StartDateForm': startingDateForm,
+                'EndDateForm': endingDateForm}
 
     return render(request, 'manager/sales.html', context)
+
 
 def trends(request):
     return render(request, 'manager/trends.html')
 
 
 # Function for getting the whole history of sales
-def getYearHistory():
+# By default, gives the year
+def getSalesReport(startDate, endDate=timezone.now().date()):
     with connection.cursor() as cursor:
-        endTime = timezone.now().date()
-        startTime = endTime - timedelta(days = 365)
-
         # Queries for all items within the year
         sqlCommand = ("SELECT menu_items.id, menu_items.price, menu_items.description, menu_items.category, " +
                         "menu_items.times_ordered, SUM(order_breakout.food_items) AS total_quantity_ordered " +
                         "FROM orders JOIN order_breakout ON orders.id = order_breakout.order_id JOIN menu_items " +
                         "ON order_breakout.food_items = menu_items.id WHERE orders.order_time BETWEEN %s AND %s " +
                         "GROUP BY menu_items.id, menu_items.description, menu_items.times_ordered, menu_items.price;")
-        cursor.execute(sqlCommand, [startTime, endTime])
+        cursor.execute(sqlCommand, [startDate, endDate])
         cursorOutput = cursor.fetchall()
 
         # Sorts and places all the items into the context
@@ -82,3 +102,11 @@ def getYearHistory():
                        for currentItem in dataSorted]
         
         return dataReport
+
+
+# Creates classes for date submissions
+class StartDateForm(forms.Form):
+    startDate = forms.DateField()
+
+class EndDateForm(forms.Form):
+    endDate = forms.DateField()
