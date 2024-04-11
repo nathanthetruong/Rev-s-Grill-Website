f4d0463d18ac46b5fd1bffe5e09db5077d0f4af4
AlexDo12 <alexthanhdo2003@gmail.com>
2024-04-10 22:48:56 -0500

    Local OAuth functionality

--- RevsDjango/RevsDjango/urls.py
+++ RevsDjango/RevsDjango/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('menu/', include('menu.urls')),
     path('orders/', include('orders.urls')),
     path('menuboard/', include('menuboard.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
