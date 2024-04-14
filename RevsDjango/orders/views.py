98dc81d8734e78babe3d26a2db67e1be93af5a74
nathanthetruong <t.nathan235@gmail.com>
2024-04-14 01:40:59 -0500

    Implemented full order database handling

--- RevsDjango/orders/views.py
+++ RevsDjango/orders/views.py
@@ -2,17 +2,11 @@ import json
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
-from django.db import connection
+from django.db import connection, transaction, IntegrityError
 from django.utils import timezone
 from django.contrib import messages
+import time
 
-# Class to store all information on a menu item
-class MenuItem:
-    def __init__(self, description, price, id, category):
-        self.description = description
-        self.price = price
-        self.id = id
-        self.category = category
 
 # Initializes all the menu items buttons
 def orders(request):
@@ -25,7 +19,12 @@ def orders(request):
         data = cursor.fetchall()
         data.sort()
         buttonData = [{'description': currentItem[0], 'price': currentItem[1],
-                        'category': currentItem[2]} for currentItem in data]
+                        'category': currentItem[2], 'id': currentItem[3]} for currentItem in data]
+        
+        menuItems = {currentItem[3]: {'description': currentItem[0], 'price': currentItem[1],
+                        'category': currentItem[2]} for currentItem in data}
+
+        request.session['menuItems'] = menuItems
 
         # Categorize buttons based on their descriptions
         categorized_buttons = {
@@ -38,15 +37,16 @@ def orders(request):
         }
 
         for button in buttonData:
-            if button['category'] == 'Burger':
+            category = button['category']
+            if category == 'Burger':
                 categorized_buttons['Burgers'].append(button)
-            elif button['category'] == 'Value Meal':
+            elif category == 'Value Meal':
                 categorized_buttons['Baskets'].append(button)
-            elif button['category'] == 'Sandwiches':
+            elif category == 'Sandwiches':
                 categorized_buttons['Sandwiches'].append(button)
-            elif button['category'] == 'Shakes/More':
+            elif category == 'Shakes/More':
                 categorized_buttons['Shakes'].append(button)
-            elif button['category'] == 'Drink':
+            elif category == 'Drink':
                 categorized_buttons['Beverages'].append(button)
             else:
                 categorized_buttons['Sides'].append(button)
@@ -62,16 +62,19 @@ def cart_view(request):
 def addItem(request):
     if request.method == 'POST':
         price = float(request.POST.get('price'))
-        description = request.POST.get('description')
+        id = request.POST.get('id')
 
         # Use a session so we don't have concurrency issues with global variables breaking heroku
         # Retrieve the cart from the session, add new price to total, then update cart 
-        cart = request.session.get('cart', {})
-        cart[description] = cart.get(description, 0) + price
+        if 'cart' not in request.session:
+            request.session['cart'] = {'total_price': 0.0, 'ids': []}
+        cart = request.session.get('cart')
+        cart['total_price'] += price
+        totalPrice = cart['total_price']
+        cart['ids'].append(id)
         request.session['cart'] = cart
-        total_price = sum(cart.values())
 
-        return JsonResponse({'cart_count': len(cart), 'total_price': total_price})
+        return JsonResponse({'cart_count': len(cart['ids']), 'total_price': totalPrice})
     
     return JsonResponse({'error': 'failed'}, status=400)
 
@@ -83,23 +86,73 @@ def checkout(request):
         employeeId = 1111
         orderTime = timezone.now()
 
-        cart = request.session.get('cart', {})
-        totalPrice = sum(cart.values())
-        descriptions = cart.keys()
+        cart = request.session.get('cart')
+        totalPrice = cart['total_price']
 
+        # Loops until the order is processed fully in the database successfully
+        while (True):
+            with transaction.atomic():
+                try:
+                    orderId = getNewOrderID()
+                    updateOrders(customerId, employeeId, totalPrice, orderTime, orderId)
+                    for currentId in cart['ids']:
+                        ingredientIds = getUsedInventoryItems(orderId, currentId)
+                        updateInventory(ingredientIds)
 
+                    break
 
-        # Get a new valid ID for the order
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT MAX(id) FROM orders")
-            orderID = cursor.fetchone()[0] + 1
-
-        # Insert into orders table
-        with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO orders (id, customer_id, employee_id, total_price, order_time) VALUES (%s, %s, %s, %s, %s)", [orderID, customerId, employeeId, totalPrice, orderTime])
+                # Waits for 0.1 seconds before retrying order submission
+                except IntegrityError:
+                    time.sleep(0.1)
 
         # Reset price
         del request.session['cart']
 
         messages.success(request, 'Success')
-        return redirect('Revs-Order-Screen') 
+        return redirect('Revs-Order-Screen')
+    
+
+# Handles getting a new order ID
+def getNewOrderID():
+    # Get a new valid ID for the order
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT MAX(id) FROM orders")
+        orderID = cursor.fetchone()[0] + 1
+    
+    return orderID
+
+# Handles updating the orders table
+def updateOrders(customerId, employeeId, totalPrice, orderTime, orderId):
+    # Inputs the order information into the orders table
+    with connection.cursor() as cursor:
+        sqlCommand = ("INSERT INTO orders (id, customer_id, employee_id, total_price, order_time) " +
+                    "VALUES (%s, %s, %s, %s, %s)")
+        cursor.execute(sqlCommand, [orderId, customerId, employeeId, totalPrice, orderTime])
+
+# Gets a list of all inventory items needed to be updated
+def getUsedInventoryItems(orderId, currentId):
+    # Handles insertion into Order Breakout
+    with connection.cursor() as cursor:
+        sqlCommand = "INSERT INTO order_breakout (order_id, food_items) VALUES (%s, %s)"
+        cursor.execute(sqlCommand, [orderId, currentId])
+
+    # Handles incrementing times_ordered for the menu item
+    with connection.cursor() as cursor:
+        sqlCommand = "UPDATE menu_items SET times_ordered = times_ordered + 1 WHERE id = %s"
+        cursor.execute(sqlCommand, [currentId])
+
+    # Finds all the ingredients to be updated in the inventory table
+    with connection.cursor() as cursor:
+        sqlCommand = "SELECT inventory_id FROM food_to_inventory WHERE food_item_id = %s"
+        cursor.execute(sqlCommand, [currentId])
+        ingredientIds = cursor.fetchall()
+
+    return ingredientIds
+
+# Handles updating all the items in a single menu item
+def updateInventory(ingredientIds):
+    # Updates all the ingredient's quantity in the inventory table
+    for currentIngredientID in ingredientIds:
+        with connection.cursor() as cursor:
+            sqlCommand = "UPDATE inventory SET quantity_remaining = quantity_remaining - 1 WHERE id = %s"
+            cursor.execute(sqlCommand, [currentIngredientID])
