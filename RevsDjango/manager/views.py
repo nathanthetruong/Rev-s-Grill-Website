76effb4a734a7fb84f3da56fcbb40a9ed6ba7fc3
AlexDo12 <alexthanhdo2003@gmail.com>
2024-04-14 13:41:49 -0500

    Reimplement excess after fixing db issues

--- RevsDjango/manager/views.py
+++ RevsDjango/manager/views.py
@@ -46,7 +46,70 @@ def restock(request):
     return render(request, 'manager/restock.html')
 
 def excess(request):
-    return render(request, 'manager/excess.html')
+    startingDate = timezone.now().date()-timedelta(days=365)
+    endingDate = timezone.now().date()
+    startingDateForm = StartDateForm()
+    endingDateForm = EndDateForm()
+    if request.method == "POST":
+        if "submit" in request.POST:
+            startingDateForm = StartDateForm(request.POST)
+            endingDateForm = EndDateForm(request.POST)
+
+            # If the date is valid, extracts the selected date
+            if startingDateForm.is_valid():
+                startingDate = startingDateForm.cleaned_data['startDate']
+            if endingDateForm.is_valid():
+                endingDate = endingDateForm.cleaned_data['endDate']
+            print(f"Received dates: Start - {startingDate}, End - {endingDate}")  
+
+    excess_report = getExcessReport(startingDate, endingDate)
+    #print(f"Query returned {len(excess_report)} items")  
+    print(excess_report)
+
+    # Default option
+    context = {'excess_report': excess_report,
+                'StartDateForm': startingDateForm,
+                'EndDateForm': endingDateForm}
+
+    return render(request, 'manager/excess.html', context)
+
+
+def getExcessReport(startDate, endDate):
+    with connection.cursor() as cursor:
+        test_startDate = '2023-04-20'
+        test_endDate = '2023-04-21'
+        # Queries for available inventory items still below their quantity target between two dates
+        sqlCommand = ("""
+                        SELECT inv.id AS inventory_id, inv.description AS inventory_description, inv.quantity_target, COALESCE(SUM(fti.quantity), 0) AS quantity_consumed, inv.quantity_target * 0.1 AS ten_percent_target
+                        FROM inventory inv
+                        LEFT JOIN food_to_inventory fti ON inv.id = fti.inventory_id
+                        LEFT JOIN menu_items mi ON fti.food_item_id = mi.id
+                        LEFT JOIN order_breakout ob ON mi.id = ob.food_items
+                        LEFT JOIN orders o ON ob.order_id = o.id 
+                        WHERE o.order_time BETWEEN %s AND %s
+                        GROUP BY inv.id, inv.description, inv.quantity_target
+                        HAVING COALESCE(SUM(fti.quantity), 0) < inv.quantity_target * 0.1;                      
+                      """)
+        cursor.execute(sqlCommand, [startDate, endDate])
+        cursorOutput = cursor.fetchall()
+
+        """
+        print("Executing SQL:", sqlCommand)
+        print("With parameters:", startDate, endDate)
+        print("Query Results:", cursorOutput)
+        """
+
+        # Sorts and places all the items into the context
+        dataSorted = sorted(cursorOutput, key=lambda x: x[0])
+        dataReport =[{'inventory_id': currentItem[0], 
+                      'inventory_description': currentItem[1],
+                      'quantity_target': currentItem[2],
+                      'quantity_consumed': currentItem[3],
+                      'ten_percent_target': currentItem[4]}
+                       for currentItem in dataSorted]
+        
+        print(dataReport)
+        return dataReport
 
 def productusage(request):
     return render(request, 'manager/productusage.html')
@@ -93,6 +156,10 @@ def getSalesReport(startDate, endDate=timezone.now().date()):
                         "GROUP BY menu_items.id, menu_items.description, menu_items.times_ordered, menu_items.price;")
         cursor.execute(sqlCommand, [startDate, endDate])
         cursorOutput = cursor.fetchall()
+
+        print("Executing SQL:", sqlCommand)
+        print("With parameters:", startDate, endDate)
+        print("Query Results:", cursorOutput)
 
         # Sorts and places all the items into the context
         dataSorted = sorted(cursorOutput, key=lambda x: x[0])
