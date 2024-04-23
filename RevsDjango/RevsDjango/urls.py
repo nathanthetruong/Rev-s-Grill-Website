1e81226e73b56a07eab9a7a5ea123407da1ef968
AlexDo12 <alexthanhdo2003@gmail.com>
2024-04-22 23:17:09 -0500

    Cashier app framework

--- RevsDjango/RevsDjango/urls.py
+++ RevsDjango/RevsDjango/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('menu/', include('menu.urls')),
     path('orders/', include('orders.urls')),
     path('menuboard/', include('menuboard.urls')),
+    path('cashier/', include('cashier.urls')),
     path('accounts/', include('allauth.urls')),
     path('social/', include('allauth.socialaccount.urls')),
 ]
