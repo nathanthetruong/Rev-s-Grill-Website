1626099aee700fd2b945a20f2952ea0719d51a93
uzmahamid01 <uzma_hamid@tamu.edu>
2024-04-26 05:01:14 -0500

    menuboard frontend backend done needs testing and need to add page rendering

--- RevsDjango/menuboard/views.py
+++ RevsDjango/menuboard/views.py
@@ -1,6 +1,69 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse
+from django.db import connection, transaction, IntegrityError
+from django.utils import timezone
+from django.contrib import messages
+from collections import defaultdict
+import time
 
-# Create your views here.
+
+# Initializes all the menu items buttons
+
 def menu_board(request):
-    return render(request, 'menuboard/menuboard.html')
+    with connection.cursor() as cursor:
+        if 'cart' in request.session:
+            del request.session['cart']
+
+        cursor.execute("SELECT description, price, category, id FROM menu_items")
+        data = cursor.fetchall()
+        data.sort()
+        menuItems = [{'description': currentItem[0], 'price': currentItem[1],
+                        'category': currentItem[2], 'id': currentItem[3],
+                        'count': 1} for currentItem in data]
+
+        request.session['menuItems'] = menuItems
+
+        categorizedButtons = {
+            'Burgers': [],
+            'Baskets': [],
+            'Sandwiches': [],
+            'Shakes': [],
+            'Beverages': [],
+            'Sides': []
+        }
+
+        for button in menuItems:
+            category = button['category']
+            if category == 'Burger':
+                categorizedButtons['Burgers'].append(button)
+            elif category == 'Value Meal':
+                categorizedButtons['Baskets'].append(button)
+            elif category == 'Sandwiches':
+                categorizedButtons['Sandwiches'].append(button)
+            elif category == 'Shakes/More':
+                categorizedButtons['Shakes'].append(button)
+            elif category == 'Drink':
+                categorizedButtons['Beverages'].append(button)
+            else:
+                categorizedButtons['Sides'].append(button)
+
+        context = {'categorizedButtons': categorizedButtons}
+
+        return render(request, 'menuboard/menuboard.html', context)
+
+
+
+# def login(request):
+#     # Your logic for the login page
+#     return render(request, 'login.html')
+
+# def orders(request):
+#     # Your logic for the login page
+#     return render(request, 'orders.html')
+
+
+def help(request):
+    # Your logic for the help page
+    return render(request, 'help.html')
