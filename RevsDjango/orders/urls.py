d387c6685f1d0c7e4f2c4ad77458947a27a31c37
uzmahamid01 <uzma_hamid@tamu.edu>
2024-04-28 19:58:54 -0500

    cleaned code and orders page bug fixed

--- RevsDjango/orders/urls.py
+++ RevsDjango/orders/urls.py
@@ -13,4 +13,8 @@ urlpatterns = [
     path('orders/', views.transactionView, name='orders'),
     path('getCartItems/', views.getCartItems, name='getCartItems'),
     path('login/', views.loginView, name='login'),
+    path('removeItem/', views.removeItem, name='removeItem'),
+    path('updateQuantity/', views.updateQuantity, name='updateQuantity'),
+    
+
 ]
