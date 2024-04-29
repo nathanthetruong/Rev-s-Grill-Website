493bd9dd1b7947ea631ec7dd9e00906723d0cb83
AlexDo12 <alexthanhdo2003@gmail.com>
2024-04-28 23:15:45 -0500

    Fix wrong variable name

--- RevsDjango/manager/views.py
+++ RevsDjango/manager/views.py
@@ -94,8 +94,8 @@ def modifyItem(request):
         menu_item.description = description
         menu_item.category = category
         menu_item.times_ordered = times_ordered
-        menu_item.startDate = startDate
-        menu_item.endDate = endDate
+        menu_item.start_date = startDate
+        menu_item.end_date = endDate
         menu_item.save()
     return redirect('Revs-Manager-Screen')
 
